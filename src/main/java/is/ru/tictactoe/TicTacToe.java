d0383f3f79fe11126fba816f3b58ff486348df2e
Ólafur Helgi Jónsson <olafurj13@i-7965f214.greenqloud.internal>
2014-10-25 14:09:49 +0000

    Added Board function and added getRowCount() function to TicTacToe.java. Also added a test for getRowCount().

--- src/main/java/is/ru/tictactoe/TicTacToe.java
+++ src/main/java/is/ru/tictactoe/TicTacToe.java
@@ -1,7 +1,27 @@
 package is.ru.tictactoe;
 
 public class TicTacToe{
+    public static final int ONE = 1;
+    public static final int TWO = 2;
+    public static final int THREE = 3;
+    public static final int FOUR = 4;
+    public static final int FIVE = 5;
+    public static final int SIX = 6;
+    public static final int SEVEN = 7;
+    public static final int EIGHT = 8;
+    public static final int NINE = 9;
+    public static final char EMPTY = ' ';    
 
+    private static char[][] board;
+
+    public TicTacToe(){
+    	board = new char[5][5];
+	for(int i = 0; i < board.length; i++){
+	    for(int j = 0; j < board[i].length; j++){
+		    board[i][j] = EMPTY;
+		}
+    	}
+    }
     public static void main(String[] args)
     {
 	test();
@@ -9,5 +29,10 @@ public class TicTacToe{
     
     public static String test(){
     	return("TicTacToe");
+    }
+
+    public static int getRowCount(){
+    	TicTacToe tc = new TicTacToe();
+    	return tc.board.length;
     }
 }
--- src/test/java/is/ru/tictactoe/TicTacToeTest.java
+++ src/test/java/is/ru/tictactoe/TicTacToeTest.java
@@ -4,12 +4,16 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 public class TicTacToeTest {
-   public static void main(String args[]){
-   	org.junit.runner.JUnitCore.main("is.ru.tictactoe.TicTacToeTest");
-   }
+    public static void main(String args[]){
+    	org.junit.runner.JUnitCore.main("is.ru.tictactoe.TicTacToeTest");
+    }
    
-   @Test
-   public void testEmptyString(){
-        assertEquals("TicTacToe", TicTacToe.test());
-   }
+    @Test
+    public void testEmptyString(){
+         assertEquals("TicTacToe", TicTacToe.test());
+    }
+    @Test
+    public void testHowManyRows(){
+	assertEquals(5, TicTacToe.getRowCount());
+    }
 }	
