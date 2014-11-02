9ca79c595b1852eba48424c5d87748d7a37369d9
Ólafur Helgi Jónsson <olafurj13@i-7965f214.greenqloud.internal>
2014-11-02 18:24:12 +0000

    Added TicTacToeWeb.Java, trying to get it running on heroku

---
+++ src/main/java/is/ru/tictactoe/TicTacToeWeb.java
@@ -0,0 +1,32 @@
+package is.ru.TicTacToe;
+
+import spark.*;
+import static spark.Spark.*;
+import spark.servlet.SparkApplication;
+import is.ru.TicTacToe.TicTacToe;
+public class TicTacToeWeb implements SparkApplication {
+    public static void main(String[] args){
+        staticFileLocation("/public");
+        SparkApplication tictactoe = new TicTacToeWeb();
+        String port = System.getenv("PORT");
+        if (port != null) {
+            setPort(Integer.valueOf(port));
+        }
+        tictactoe.init();
+    }
+
+    private TicTacToe game;
+    
+    public void init() {
+	   if(game == null) {
+	       game = new TicTacToe();
+        }
+        post(new Route("/playGame") {
+            @Override
+            public Object handle(Request request, Response response) {
+                game = new TicTacToe();
+                return true;
+            }
+        });
+    }
+}
